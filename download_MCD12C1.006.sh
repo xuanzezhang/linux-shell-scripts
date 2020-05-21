51a9aa7f7f121b8dc6e3a146167a25534901755d
Andrew X. Zhang <xuanzezhang@hotmail.com>
2020-05-21 17:14:53 +0800

    Add files via upload

---
+++ download_MCD12C1.006.sh
@@ -0,0 +1,1 @@
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies -np -r --content-disposition https://e4ftl01.cr.usgs.gov/MOTA/MCD12C1.006/
